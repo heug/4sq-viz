d6e69bf4a9c679373e82e06159e6e657b740f947
Eugene Choe <eugene.choe@gmail.com>
2017-02-18 16:41:59 -0800

    Rename stubs Create obj with checkins by category and name detail

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
-import STUB1 from 'stubs/checkIn1.json';
-import STUB2 from 'stubs/checkIn2.json';
-import STUB3 from 'stubs/checkIn3.json';
-import STUB4 from 'stubs/checkIn4.json';
+const STUB1 = require('./stubs/checkIn1');
+const STUB2 = require('./stubs/checkIn2');
+const STUB3 = require('./stubs/checkIn3');
+const STUB4 = require('./stubs/checkIn4');
 
+let checkInList = STUB1.response.checkins.items
+	.concat(STUB2.response.checkins.items)
+	.concat(STUB3.response.checkins.items)
+	.concat(STUB4.response.checkins.items);
+
+console.log(checkInList[10].venue.name);
+
+var store = {};
+
+for (var i = 0; i < checkInList.length; i++) {
+	if (checkInList[i].venue.categories[0]) {
+		if (!store[checkInList[i].venue.categories[0].pluralName]) {
+			store[checkInList[i].venue.categories[0].pluralName] = {
+				venues: {},
+				visitCount: 1
+			};
+		} else {
+			store[checkInList[i].venue.categories[0].pluralName].visitCount++;
+		}
+		if (!store[checkInList[i].venue.categories[0].pluralName].venues[checkInList[i].venue.name]) {
+			store[checkInList[i].venue.categories[0].pluralName].venues[checkInList[i].venue.name] = 1;
+		} else {
+			store[checkInList[i].venue.categories[0].pluralName].venues[checkInList[i].venue.name]++;
+		}
+	}
+};
+
+for (var key in store) {
+	console.log(key, store[key].visitCount);
+};
