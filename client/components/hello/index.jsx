f8cc56952dfe4d4c353409afbab298466ee78b2a
Eugene Choe <eugene.choe@gmail.com>
2017-03-04 17:07:00 -0800

    Implement mapping venues function

--- client/components/hello/index.jsx
+++ client/components/hello/index.jsx
@@ -18,21 +18,40 @@ class Hello extends React.Component {
 		this.props.getAccount();
 	}
 
-	displayList(obj) {
-		// for (var key in obj) {
-		// 	console.log(key);
-		// }
-		// if (Object.keys(this.props.))
+	displayVenues(cat) {
+		console.log('inside fn', this.props.account['Diners'].venues);
+		console.log('venue list', Object.keys(this.props.account[cat].venues));
+		if (!this.props.account[cat] || this.props.account[cat].length === 0) {
+			return (
+				<h2>No Venues!</h2>
+			);
+		}
+		return (
+			Object.keys(this.props.account[cat].venues).map((venue) => {
+				return (
+					<li>
+						{ venue }
+					</li>
+				);
+			})
+		);
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>HELLO WORLD</h1>
-				{ this.props.account.account ? 
-					this.props.account.account['Airports'].visitCount : 
+				{ this.props.account ? 
+					this.props.account['Diners'].visitCount : 
 					'not loaded' 
 				}
+				<br/>
+				<ul>
+					{ this.props.account ? 
+						this.displayVenues('Bars') :
+						'not loaded'
+					}
+				</ul>
 				<svg width="1000" height="800"></svg>
 			</div>
 		);
@@ -41,7 +60,7 @@ class Hello extends React.Component {
 
 const mapStateToProps = (state) => {
 	return {
-		account: state.account
+		account: state.account.account
 	};
 };
 
