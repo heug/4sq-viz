b0a63241f5c4137679cefd839e8007beef788934
Eugene Choe <eugene.choe@gmail.com>
2017-03-06 12:59:45 -0800

    Create landing page Add input fields for e-mail/password

--- client/routes.jsx
+++ client/routes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, IndexRoute, Route } from 'react-router';
 import Hello from './components/hello/';
+import Landing from './components/landing/';
 
 const Routes = (props) => (
 	<Router {...props}>
-		<Route path='/' component={Hello} />
+		<Route path='/' component={Landing} />
+		<Route path='hello' component={Hello} />
 	</Router>
 );
 
