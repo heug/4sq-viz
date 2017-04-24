cf5033a1fa85ecf456e5ce92e5a2676392a9e3ac
Eugene Choe <eugene.choe@gmail.com>
2017-04-24 11:51:55 -0700

    Add Unit tests for data helpers Update package.json with properly formatted project name

--- test/data-helpers/app.spec.js
+++ test/data-helpers/app.spec.js
@@ -8,5 +8,54 @@ describe('Checkin Helper Functions', function() {
 			const tester = DATA_HELPERS.getCategoryCount(STUB_DATA.response.checkins.items);
 			expect(tester['Portuguese Restaurant'].count).to.equal(2);
 		});
+		it('totals all venues', function() {
+			const tester = DATA_HELPERS.getCategoryCount(STUB_DATA.response.checkins.items);
+			let venueCount = tester['Portuguese Restaurant'].count + tester['Cocktail Bar'].count
+			expect(venueCount).to.equal(3);
+		});
+	});	
+	describe('Geojson', function() {
+		it('contains the appropriate type header', function() {
+			const tester = DATA_HELPERS.getGeojson(STUB_DATA.response.checkins.items);
+			expect(tester['type']).to.equal('FeatureCollection');
+		});
+		it('features are contained in an array', function() {
+			const tester = DATA_HELPERS.getGeojson(STUB_DATA.response.checkins.items);
+			expect(tester['features']).to.be.an('array');
+		});
+		describe('geometry', function() {
+			it('each feature has a geometry type', function() {
+				const tester = DATA_HELPERS.getGeojson(STUB_DATA.response.checkins.items);
+				let featurePoints = true;
+				tester['features'].forEach((feature) => {
+					if (!feature.geometry.type) {
+						featurePoints = feature;
+					}
+				});
+				expect(featurePoints).to.equal(true);
+			});
+			it('each feature has a coordinates array', function() {
+				const tester = DATA_HELPERS.getGeojson(STUB_DATA.response.checkins.items);
+				let featureCoords = true;
+				tester['features'].forEach((feature) => {
+					if (feature.geometry.coordinates.length !== 2) {
+						featureCoords = feature;
+					}
+				});
+				expect(featureCoords).to.equal(true);
+			});
+		});
+		describe('properties', function() {
+			it('each feature has a property object', function() {
+				const tester = DATA_HELPERS.getGeojson(STUB_DATA.response.checkins.items);
+				let featureProps = true;
+				tester['features'].forEach((feature) => {
+					if (!feature.properties) {
+						featureProps = feature;
+					}
+				});
+				expect(featureProps).to.equal(true);
+			});
+		});
 	});	
 });
