0bda384aecd5654278deda3addd7fc526e23190a
Eugene Choe <eugene.choe@gmail.com>
2017-04-23 23:13:04 -0700

    Implement first test for data helper functions

--- test/data-helpers/app.spec.js
+++ test/data-helpers/app.spec.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { expect } from 'chai';
-// import { Ci } from '../../client/components/ci/';
+import DATA_HELPERS from '../../data-helpers/app';
+import STUB_DATA from '../../stubs/checkInTester';
 
-// describe('Trivial Application', function() {
-// 	describe('Button', function() {
-// 		it('toggles text state on click', function() {
-// 			const tester = mount(<Ci />);
-// 			expect(tester.state('showText')).to.equal(false);
-// 			tester.find('#btn-1').simulate('click');
-// 			expect(tester.state('showText')).to.equal(true);
-// 			tester.find('#btn-1').simulate('click');
-// 			expect(tester.state('showText')).to.equal(false);
-// 		});
-// 		it('should display text when clicked', function() {
-// 			const tester = mount(<Ci />);
-// 			expect(tester.find('#sentence').text()).to.equal('');
-// 			tester.find('#btn-1').simulate('click');
-// 			expect(tester.find('#sentence').text()).to.equal('CircleCI is Continuous Integration');
-// 		});
-// 	});	
-// });
+describe('Checkin Helper Functions', function() {
+	describe('Venues', function() {
+		it('aggregates categories with counts', function() {
+			const tester = DATA_HELPERS.getCategoryCount(STUB_DATA.response.checkins.items);
+			expect(tester['Portuguese Restaurant'].count).to.equal(2);
+		});
+	});	
+});
